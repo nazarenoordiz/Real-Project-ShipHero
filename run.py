980fbcb6732f4bb5e4b610709ae8ff0332533513
Nazareno Ordiz <no@bb.vision>
2021-11-07 14:33:59 -0300

    change

--- run.py
+++ run.py
@@ -1,28 +1,33 @@
 from flask import Flask, redirect, url_for, render_template, request, render_template_string
 import graphene
+from graphene import ObjectType, String, Schema
 import xmltodict, json
 import requests
-from werkzeug.datastructures import Headers
 
 app = Flask(__name__)
 
 
-class Query(graphene.ObjectType):
-    
-    rates = graphene.String(
-        label_cost=graphene.Argument(graphene.String, default_value='test'),
-        service_used=graphene.Argument(graphene.String, default_value='testing'), 
-        label_pdf=graphene.Argument(graphene.String, default_value='tesint')
-        )
+class Query(ObjectType):
+    # this defines a Field `hello` in our Schema with a single Argument `name`
+    rate = String(
+        cose=String(default_value="ERROR"),
+        service_name=String(default_value="ERROR"),
+        service_used=String(default_value="ERROR"),
+        label_pdf=String(default_value="ERROR"),
+    )
+    goodbye = String()
 
-    def resolve_rates(self, args, context, info):
-        return 'Your cose: {}, the service name and code: {}-{}, and the transittime: {}'.format(
-            args['label_cost'], args['service_used'], args['label_pdf']
-        )
+    # our Resolver method takes the GraphQL context (root, info) as well as
+    # Argument (name) for the Field and returns data for the query Response
+    def resolve_rate(root, info, cose, service_name, service_used, label_pdf):
+        return f'Your cose: {cose}, the service name and code: {service_name}-{service_used}, and the transittime: {label_pdf}'
+
+    def resolve_goodbye(root, info):
+        return 'See ya!'
+
 
 
 schema = graphene.Schema(query=Query)
-
 @app.route("/rates", methods=['POST'])
 
 def rates():
@@ -41,8 +46,20 @@ def rates():
         account_number=510087100,
         meter_number=119242929)
     req = requests.post(url, data=post_data, headers=headers)
-    
-    return req.content
+    obj = xmltodict.parse(req.content)
+    ServiceDescription = obj["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["RateReply"]["RateReplyDetails"]["ServiceDescription"]
+    response_dict = {
+        'cose': 'None',
+        'service_name': str(ServiceDescription['Description']),
+        'service_code': str(ServiceDescription['Code']),
+        'transit_time': None
+    }
+    query_string = '{{ rate(service_name: "{name}" cose: "{cose}") }}'.format(
+        cose=response_dict['cose'], name=response_dict['service_name'])
+    result = schema.execute(query_string)
+    return result.data['rate']
+
+
 
 #    necesitamos pegarle a la api de fedex, retornar los datos, pasarlos por graphql, y hacer la response que necesitamos :v
 
